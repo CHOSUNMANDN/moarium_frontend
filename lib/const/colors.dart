4ab8eb291036beb2976d961c531b400687845900
Ag <choeun8339@naver.com>
2024-07-05 16:17:59 +0900

    [Feat] 상수 파일 생성 및 색상 상수 파일 생성
    - 상수 파일을 생성하였습니다.
    - 프로젝트에 사용될 색상들을 모아둔 색상 상수 파일을 생성하였습니다.

---
+++ lib/const/colors.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+/// moarium_frontend 색상 상수 파일
+/// 상수: lowerCamelCase
+///
+/// 참고 문헌
+/// https://dart.dev/effective-dart/style#dont-use-prefix-letters
+
+// 배경
+// 배경 색상: 0xFFFFFFFF
+const backgroundColor = Colors.white;
+// 일반 글자 색상: 0xFF8B8B8B
+const primaryTextColor = Color(0xFF8B8B8B);
+
+// 버튼
+// 버튼 배경 색상: 0xFF31C1FF
+const buttonColor = Color(0xFF31C1FF);
+// 버튼 글자 색상: 0xFFFFFFFF
+const buttonTextColor = Colors.white;
+
+// 하단 네비게이션 바
+// 하단 네비게이션 바 배경 색상: 0xFFD9D9D9
+const bottomNavigationBarBackgroundColor = Color(0xFFD9D9D9);
+// 하단 네비게이션 바 글자 색상: 0xFF000000
+const bottomNavigationBarTextColor = Colors.black;
+
+// 모달창 (일반)
+// 모달창 배경 색상: 0xFFD9D9D9
+const modalBackgroundColor = Color(0xFFD9D9D9);
+// 모달창 글자 색상: 0xFF8B8B8B
+const modalTextColor = Color(0xFF8B8B8B);
+
+// 모달창 (경고)
+// 모달창 배경 색상: 0xFFFFC3C3
+const modalAlertBackgroundColor = Color(0xFFFFC3C3);
+// 모달창 글자 색상: 0xFFFF0000
+const modalAlertTextColor = Color(0xFFFF0000);
