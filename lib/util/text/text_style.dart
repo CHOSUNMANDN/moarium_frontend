aa1d71e367ea3fe790182dd561ce3934dfe14d83
Ag <choeun8339@naver.com>
2024-07-09 22:22:35 +0900

    [Feat] text style 상수 파일 추가
    - 프로젝트에 사용되는 text style 상수 파일을 추가하였습니다.

---
+++ lib/util/text/text_style.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+final TextStyle SEMIBOLD_X_LARGE = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.w600,
+  fontSize: 32.0.sp,
+  letterSpacing: (-2.5 / 100) * 32.sp,
+  height: 44 / 32,
+);
+
+final TextStyle SEMIBOLD_LARGE = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.w600,
+  fontSize: 20.0.sp,
+  letterSpacing: (-2.5 / 100) * 20.sp,
+  height: 32 / 20,
+);
+
+final TextStyle SEMIBOLD_MEDIUM = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.w600,
+  fontSize: 16.0.sp,
+  letterSpacing: (-2.5 / 100) * 16.sp,
+  height: 22 / 16,
+);
+
+final TextStyle SEMIBOLD_SMALL = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.w600,
+  fontSize: 14.0.sp,
+  letterSpacing: (-2.5 / 100) * 14.sp,
+  height: 22 / 14,
+);
+
+final TextStyle MEDIUM_MEDIUM = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.w500,
+  fontSize: 16.0.sp,
+  letterSpacing: (-2.5 / 100) * 16.sp,
+  height: 24 / 16,
+);
+
+final TextStyle REGULAR_SMALL = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.w400,
+  fontSize: 14.0.sp,
+  letterSpacing: (-2.5 / 100) * 14.sp,
+  height: 22 / 14,
+);
