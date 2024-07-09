bd857939ed8465a49b57f7a9307675a8f485e95a
Ag <choeun8339@naver.com>
2024-07-09 20:07:21 +0900

    [Refactor] 주로 사용하는 색상이 위로 오도록 상수 수정
    - 주로 사용하는 색상 순으로 나열되도록 수정했습니다.

--- lib/util/color/colors.dart
+++ lib/util/color/colors.dart
@@ -1,16 +1,20 @@
+// moarium_frontend 색상 상수 파일
+//
+// 주로 사용 하는 색상 먼저 쓸 것
+//
+// 배경색, 글자색 순으로 쓸 것
+//
 import 'package:flutter/material.dart';
-
-/// moarium_frontend 색상 상수 파일
 
 // 배경
 // 배경 색상: 0xFFFFFFFF
 const BACKGROUND_COLOR = Colors.white;
 // 일반 글자 색상: 0xFF8B8B8B
 const PRIMARY_TEXT_COLOR = Color(0xFF8B8B8B);
-// 보조 글자 색상 : 0xFFD9D9D9
-const SECONDARY_TEXT_COLOR = Color(0xFFD9D9D9);
-// 보조 글자 색상2 : 0xFF000000
-const TERTIARY_TEXT_COLOR = Colors.black;
+// 보조 글자 색상 : 0xFF000000
+const SECONDARY_TEXT_COLOR = Colors.black;
+// 보조 글자 색상2 : 0xFFD9D9D9
+const TERTIARY_TEXT_COLOR = Color(0xFFD9D9D9);
 
 // 상단 바
 // 상단 바 배경 색상: 0xFFFFFFFF
