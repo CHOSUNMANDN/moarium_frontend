95f571efe0958d3d5fce9f3b847274eb11199706
Ag <choeun8339@naver.com>
2024-07-15 15:48:49 +0900

    [Feat] google 로그인 관련 환경 변수 추가 및 googleSignInProvider 기능 추가
    - google 로그인 환경 변수를 추가하였습니다.
    - google 로그인에 관련된 provider 기능을 추가하였습니다.

--- lib/common/google/google.dart
+++ lib/common/google/google.dart
@@ -1,1 +1,14 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
+final googleSignInProvider = Provider<GoogleSignIn>((ref) {
+  return GoogleSignIn(
+    scopes: [
+      'email',
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/userinfo.profile',
+    ],
+    serverClientId: dotenv.get('SERVER_CLIENT_ID'),
+  );
+});
