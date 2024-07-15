c1a0588fca55fa2b1b6c6a69c21c8b8de016f2d4
Ag <choeun8339@naver.com>
2024-07-15 23:22:39 +0900

    [Feat] text style 관련 상수 추가
    - text style 관련 상수를 추가하였습니다.

--- lib/common/const/text.dart
+++ lib/common/const/text.dart
@@ -1,50 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 final TextStyle SEMIBOLD_X_LARGE = TextStyle(
   fontFamily: 'Inter',
   fontWeight: FontWeight.w600,
-  fontSize: 32.0.sp,
-  letterSpacing: (-2.5 / 100) * 32.sp,
+  fontSize: 32.0,
+  letterSpacing: (-2.5 / 100) * 32,
   height: 44 / 32,
 );
 
 final TextStyle SEMIBOLD_LARGE = TextStyle(
   fontFamily: 'Inter',
   fontWeight: FontWeight.w600,
-  fontSize: 20.0.sp,
-  letterSpacing: (-2.5 / 100) * 20.sp,
+  fontSize: 20.0,
+  letterSpacing: (-2.5 / 100) * 20,
   height: 32 / 20,
 );
 
 final TextStyle SEMIBOLD_MEDIUM = TextStyle(
   fontFamily: 'Inter',
   fontWeight: FontWeight.w600,
-  fontSize: 16.0.sp,
-  letterSpacing: (-2.5 / 100) * 16.sp,
+  fontSize: 16.0,
+  letterSpacing: (-2.5 / 100) * 16,
   height: 22 / 16,
 );
 
 final TextStyle SEMIBOLD_SMALL = TextStyle(
   fontFamily: 'Inter',
   fontWeight: FontWeight.w600,
-  fontSize: 14.0.sp,
-  letterSpacing: (-2.5 / 100) * 14.sp,
+  fontSize: 14.0,
+  letterSpacing: (-2.5 / 100) * 14,
   height: 22 / 14,
 );
 
 final TextStyle MEDIUM_MEDIUM = TextStyle(
   fontFamily: 'Inter',
   fontWeight: FontWeight.w500,
-  fontSize: 16.0.sp,
-  letterSpacing: (-2.5 / 100) * 16.sp,
+  fontSize: 16.0,
+  letterSpacing: (-2.5 / 100) * 16,
   height: 24 / 16,
 );
 
 final TextStyle REGULAR_SMALL = TextStyle(
   fontFamily: 'Inter',
   fontWeight: FontWeight.w400,
-  fontSize: 14.0.sp,
-  letterSpacing: (-2.5 / 100) * 14.sp,
+  fontSize: 14.0,
+  letterSpacing: (-2.5 / 100) * 14,
   height: 22 / 14,
 );
