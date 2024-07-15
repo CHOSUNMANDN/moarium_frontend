ac8a159ca014fb856b73d082579c676521815d6f
Ag <choeun8339@naver.com>
2024-07-15 23:24:06 +0900

    [Feat] router 관련 기능 추가
    - router와 관련된 기능을 추가하였습니다.

--- lib/login/provider/auth_provider.dart
+++ lib/login/provider/auth_provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 import 'package:moarium_frontend/login/provider/user_me_provider.dart';
 import 'package:moarium_frontend/login/model/user_model.dart';
+import 'package:moarium_frontend/login/view/login_screen.dart';
 
 final authProvider = ChangeNotifierProvider((ref) => AuthNotifier(ref: ref));
 
@@ -21,5 +23,34 @@ class AuthNotifier extends ChangeNotifier {
 
   void logout() {
     ref.read(userMeProvider.notifier).logout();
+  }
+
+  List<GoRoute> get routes => [
+    GoRoute(
+      path: '/login',
+      name: LoginScreen.routeName,
+      builder: (_, __) => LoginScreen(),
+    ),
+  ];
+
+  String? redirectLogic(BuildContext context, GoRouterState state) {
+    final UserModelBase? user = ref.read(userMeProvider);
+
+    final logginIn = state.uri.toString() == '/login';
+
+    if (user == null) {
+      return logginIn ? null : '/login';
+    }
+
+    if (user is UserModel) {
+      return logginIn || state.uri.toString() == '/splash' ? '/' : null;
+    }
+
+    // UserModelError
+    if (user is UserModelError) {
+      return !logginIn ? '/login' : null;
+    }
+
+    return null;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import 'package:moarium_frontend/common/provider/go_router.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  runApp(
+    ProviderScope(
+      child: _App(),
+    ),
+  );
+}
+
+class _App extends ConsumerWidget {
+  const _App({Key? key}) : super(key : key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+
+    return MaterialApp.router(
+
+      theme: ThemeData(
+        fontFamily: 'Inter',
+      ),
+
+      debugShowCheckedModeBanner: false,
+      routerDelegate: router.routerDelegate,
+      routeInformationParser: router.routeInformationParser,
+      routeInformationProvider: router.routeInformationProvider,
+    );
+  }
+}
