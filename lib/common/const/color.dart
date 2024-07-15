d57de2494b571bad2e59b0ee023aa3c7ac946140
Ag <choeun8339@naver.com>
2024-07-15 16:31:54 +0900

    [Feat] color, text 파일 상수 추가
    - 프로젝트에 필요한 color, text style 관련 상수를 추가하였습니다.

---
+++ lib/common/const/color.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+// 배경
+// 배경 색상: 0xFFFFFFFF
+const BACKGROUND_COLOR = Colors.white;
+// 일반 글자 색상: 0xFF8B8B8B
+const PRIMARY_TEXT_COLOR = Color(0xFF8B8B8B);
+// 보조 글자 색상 : 0xFF000000
+const SECONDARY_TEXT_COLOR = Colors.black;
+// 보조 글자 색상2 : 0xFFD9D9D9
+const TERTIARY_TEXT_COLOR = Color(0xFFD9D9D9);
+
+// 상단 바
+// 상단 바 배경 색상: 0xFFFFFFFF
+const APP_BAR_BACKGROUND_COLOR = Colors.white;
+// 상단 바 글자 색상 : 0xFF000000
+const APP_BAR_TEXT_COLOR = Colors.black;
+
+// 버튼
+// 버튼 배경 색상: 0xFF31C1FF
+const BUTTON_BACKGROUND_COLOR = Color(0xFF31C1FF);
+// 버튼 글자 색상: 0xFFFFFFFF
+const BUTTON_TEXT_COLOR = Colors.white;
+
+// 하단 네비게이션 바
+// 하단 네비게이션 바 배경 색상: 0xFFD9D9D9
+const BOTTOM_NAVIGATION_BAR_BACKGROUND_COLOR = Color(0xFFD9D9D9);
+// 하단 네비게이션 바 글자 색상: 0xFF000000
+const BOTTOM_NAVIGATION_BAR_TEXT_COLOR = Colors.black;
+
+// 모달창 (일반)
+// 모달창 배경 색상: 0xFFD9D9D9
+const MODAL_BACKGROUND_COLOR = Color(0xFFD9D9D9);
+// 모달창 글자 색상: 0xFF8B8B8B
+const MODAL_TEXT_COLOR = Color(0xFF8B8B8B);
+
+// 모달창 (경고)
+// 모달창 배경 색상: 0xFFFFC3C3
+const MODAL_ALERT_BACKGROUND_COLOR = Color(0xFFFFC3C3);
+// 모달창 글자 색상: 0xFFFF0000
+const MODAL_ALERT_TEXT_COLOR = Color(0xFFFF0000);
